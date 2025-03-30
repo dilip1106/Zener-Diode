14d7b2a40fff62e652437b4e61a1eba4d71632b5
dilip1106 <dilipteli38@gmail.com>
2025-03-30 11:12:15 +0530

    first commit

---
+++ indexPhy4.js
@@ -0,0 +1,312 @@
+let type;
+
+wireTerminalCheck = [{ six: false , two: false},{  three: false , four: false},{one: false , five: false},{seven: false , eight: false},{nine:false,ten:false},{eleven:false,twelve:false}];
+
+terminalMap = {
+  0: "one",
+  1: "two",
+  2: "three",
+  3: "four",
+  4: "five",
+  5: "six",
+  6:"seven",
+  7:"eight",
+  8:"nine",
+  9:"ten",
+  10:"eleven",
+  11:"twelve",
+};
+
+var xValues = [65, 60, 55, 50, 45, 40, 35, 30];
+
+sequenceNum = 0;
+
+var rowData = { sno: 0, time: 0, volts: 0 };
+localStorage.setItem("rowData", JSON.stringify(rowData));
+localStorage.setItem("fullScreen", false);
+localStorage.setItem("transitionDis", false);
+var btnPressed = [false, false, false];
+
+setTimeout(() => {
+  enablingSequence(sequenceNum);
+  // startWorking();
+}, 2000);
+
+function enablingSequence(sequenceNum) {
+  // if(document.getElementById('tspanSe')){
+  // type = "silicon";
+
+  if (sequenceNum <= wireTerminalCheck.length) {
+    for (var key in wireTerminalCheck[sequenceNum]) {
+      elem = document.getElementsByClassName(key)[0];
+      elem.style.stroke = "#FFFF00";
+      elem.style.animationName = "pulse";
+      elem.style.opacity = "1";
+    }
+  }
+}
+
+function trial(componentSom) {
+  componentSomMap = terminalMap[componentSom];
+  for (var key in wireTerminalCheck[sequenceNum])
+    if (key == componentSomMap) wireTerminalCheck[sequenceNum][key] = true;
+
+  elem = document.getElementsByClassName(componentSomMap)[0];
+  // console.log(elem)
+  elem.style.animationName = "none";
+  elem.style.stroke = "none";
+  // console.log(checkPair())
+  dum = checkPair(sequenceNum);
+  // console.log(dum)
+  if (dum) {
+    wireName = "wire" + (sequenceNum + 1);
+    document.getElementById(wireName).style.transition = "display 10s";
+    document.getElementById(wireName).style.display = "block";
+    ++sequenceNum;
+    if (sequenceNum < wireTerminalCheck.length) {
+      enablingSequence(sequenceNum);
+    } else {
+      replacement();
+    }
+  }
+}
+
+function checkPair(sequenceNum) {
+  count = 0;
+  for (var key in wireTerminalCheck[sequenceNum])
+    if (wireTerminalCheck[sequenceNum][key] == true) count++;
+  // console.log(count, 'count')
+  if (count == 2) return true;
+  return false;
+}
+
+function keyPut() {
+  document.getElementById("key1").style.animation = "none";
+  document.getElementById("key1").onclick = function () {};
+  document.getElementById("keyBase1").onclick = function () {};
+}
+
+function replacement() {
+  // document.getElementById("burner-btn").style.stroke = "red";
+  // document.getElementById("burner-btn").style.strokeWidth = "1%";
+  // document.getElementById("burner-btn").onclick = function () {
+  //   document.getElementById("hotburner").style.display = "block";
+  //   checkbtnPressed(1);
+  // };
+
+  // document.getElementById("power-btn").style.stroke = "yellow";
+  // document.getElementById("power-btn").style.strokeWidth = "1%";
+  // document.getElementById("power-btn").onclick = function () {
+  //   checkbtnPressed(0);
+  // };
+
+  // document.getElementById("key1").style.display = "block";
+  // document.getElementById("key1").classList.add("key-up-down");
+  // document.getElementById("key1").onclick = function () {
+  //   checkbtnPressed(2);
+  //   keyPut();
+  // };
+  // document.getElementById("keyBase1").onclick = function () {
+  //   checkbtnPressed(2);
+  //   keyPut();
+  // };
+  document.getElementById('black-board').classList.add('hidden')
+    document.getElementById('table-board').classList.add('replacement')
+  localStorage.setItem("fullScreen", true);
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function checkbtnPressed(btnNum) {
+  btnPressed[btnNum] = true;
+  if (btnNum == 0) {
+    document.getElementById("power").textContent = "02.00";
+    document.getElementById("volt").textContent = "06.83";
+    document.getElementById("power-btn").style.strokeWidth = "0%";
+  } else if (btnNum == 1) {
+    document.getElementById("temp").textContent = "27.00";
+    document.getElementById("tempcopy").textContent = "27.00";
+    document.getElementById("burner-btn").style.strokeWidth = "0%";
+  }
+
+  if (btnPressed[0] && btnPressed[1] && btnPressed[2]) {
+    startWorking();
+  }
+}
+
+// let curarr = [
+//   6.83, 7.07, 7.31, 7.56, 7.82, 8.09, 8.36, 8.64, 8.92, 9.22, 9.52, 9.833, 10.1,
+//   10.47, 10.81, 11.15, 11.5, 11.86, 12.23, 12.61, 12.99, 13.39, 13.79, 14.21,
+//   14.63, 15.07, 15.51, 15.97, 16.44, 16.91, 17.4, 17.9, 18.41, 18.93, 19.46,
+//   20.0, 20.56, 21.13, 21.71,22.10,22.51,23.01,23.60
+// ];
+
+let curarr = [
+  7.83, 8.46, 9.01, 10.3, 11.8, 13.8, 15.2, 17.8, 19.5, 22.4, 24.6, 27.2, 28.9,
+  32.4, 36.5, 40.8, 44.3, 48.1, 52.2, 56.1, 60.9, 64.2, 68.13, 72.4, 77.9, 85.3,
+  91.4, 99.5, 102.4, 109.5, 116.7, 123.5, 126.6, 138.7, 145.4, 153.6, 158.7,
+  165.4, 171.3, 175.8, 175.8,
+];
+
+let curarrger = [0.0, 1.2];
+// let curarr = [];
+
+let curno = 6.83;
+let temprature = 27.0;
+
+let flag = 1;
+
+function startWorking() {
+  let temptext = document.getElementById("temp");
+  let temptextcopy = document.getElementById("tempcopy");
+  let curtext = document.getElementById("volt");
+  let i = 0;
+  var srno = 0;
+  let intervalId = setInterval(() => {
+    temprature++;
+    temptext.textContent = temprature.toFixed(2);
+    temptextcopy.textContent = temprature.toFixed(2);
+    if (flag == 0) {
+      curno = curarr[i++] + getRndInteger(0.5, 1.5);
+      flag = 1;
+    } else {
+      curno = curarr[i++] - getRndInteger(0.5, 1.5);
+      flag = 0;
+    }
+    if (temprature % 5 === 0.0) {
+      filldata(srno, temprature, curno);
+      srno++;
+      console.log("fill data callede");
+      console.log(curarr);
+    }
+    curtext.textContent = curno.toFixed(2);
+    // Stop the interval after reaching a certain temprature
+    if (temprature >= 68.0) {
+      clearInterval(intervalId);
+      document.getElementById("hotburner").style.display = "none";
+      // startdroping();
+    }
+  }, 1000); // Increase temprature every 100 milliseconds
+}
+
+function startdroping() {
+  let temptext = document.getElementById("temp");
+  let curtext = document.getElementById("volt");
+  let i = curarr.length - 1;
+  let flag = 1;
+  var srno = 0;
+  let intervalId = setInterval(() => {
+    temptext.textContent = temprature.toFixed(2);
+    if (flag == 0) {
+      curno = curarr[i--] + getRndInteger(0.1, 0.3);
+      flag = 1;
+    } else {
+      curno = curarr[i--] - getRndInteger(0.1, 0.3);
+      flag = 0;
+    }
+    if (temprature % 5 === 0.0) {
+      filldata(srno, temprature, curno);
+      srno++;
+      console.log("fill data callede");
+    }
+    curtext.textContent = curno.toFixed(2);
+    temprature--;
+
+    // Stop the interval after reaching a certain temprature
+    if (temprature <= 27.0) {
+      clearInterval(intervalId);
+    }
+  }, 100); // Increase temprature every 100 milliseconds
+}
+
+function filldata(srno, temprature, curno) {
+  rowData = { sno: 0, tempc: 65, curr: 0 };
+  localStorage.setItem("rowData", JSON.stringify(rowData));
+  rowData.tempc = temprature;
+  rowData.sno = srno;
+  rowData.curr = curno;
+  console.log(srno);
+  localStorage.setItem("rowData", JSON.stringify(rowData));
+}
+
+function startWorkingGer() {
+  // let temptext = document.getElementById("temp");
+  // let temptextcopy = document.getElementById("tempcopy");
+  // let curtext = document.getElementById("volt");
+  // let i=0;
+  // var srno=0;
+  // let intervalId = setInterval(() => {
+  //   temprature++;
+  //   temptext.textContent = temprature.toFixed(2);
+  //   temptextcopy.textContent = temprature.toFixed(2);
+  //   if (flag == 0) {
+  //     curno = currarrger[i++] + getRndInteger(0.5, 1.5);
+  //     flag = 1;
+  //   } else {
+  //     curno = currarrger[i++] - getRndInteger(0.5, 1.5);
+  //     flag = 0;
+  //   }
+  //   if(temprature%5===0.00){
+  //     filldata(srno,temprature,curno)
+  //     srno++
+  //     console.log("fill data callede");
+  //     console.log(currarrger)
+  //   }
+  //   curtext.textContent = curno.toFixed(2);
+  //   // Stop the interval after reaching a certain temprature
+  //   if (temprature >= 68.0) {
+  //     clearInterval(intervalId);
+  //     document.getElementById("hotburner").style.display = "none";
+  //     hi();
+  //     // startdroping();
+  //   }
+  // }, 1000);
+  alert("Germanium readings");
+}
+
+//ye wala code range ke sath ke lia hai
+
+// setTimeout(() => {
+//   rangeSelector();
+// }, 100);
+const voltarr = [0.1, 0.2, 0.3, 0.4, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75];
+const curarrforward = [0, 0, 0, 0, 0.4, 0.7, 2.3, 5.5, 15.8, 18, 18.9];
+const currreverse = [1, 1, 1, 0, 0.4, 0.7, 2.3, 15.5, 15.8, 25, 35.9];
+
+function rangeSelector() {
+  newIndexinterval = setInterval(() => {
+    const img = document.getElementById("image1-7");
+    let imgxcor = img.getAttribute("x");
+
+    let newIndex = sessionStorage.getItem("newIndex"); // Retrieve newIndex
+    newIndex = Math.floor(newIndex / 10); // Map to range [1, 10]
+
+    // Ensure newIndex stays within bounds of the array
+    if (newIndex < 1 || newIndex > 10) {
+      console.error("newIndex out of range");
+      return; // Skip this iteration if out of bounds
+    }
+
+    let volttext = document.getElementById("volt");
+    let currtext = document.getElementById("curr");
+
+    volttext.textContent = voltarr[newIndex - 1]; // Adjust for 0-based index
+    imgxcor = 77 + newIndex * 0.5;
+    img.setAttribute("x", `${imgxcor}`);
+    let curr = 0;
+    if (sessionStorage.getItem("type") === "false") {
+      curr = Math.abs(curarrforward[newIndex - 1] - getRndInteger(0.01, 0.03));
+      currtext.textContent = curr.toFixed(2);
+    } else {
+      curr = Math.abs(currreverse[newIndex - 1] - getRndInteger(0.01, 0.03));
+      currtext.textContent = curr.toFixed(2);
+    }
+    let currcopy = curr.toFixed(2);
+
+    sessionStorage.setItem("current", currcopy);
+    sessionStorage.setItem("voltage", voltarr[newIndex - 1]);
+  }, 500);
+}
+
---
+++ tablegraphPhy4.js
@@ -0,0 +1,392 @@
+currtrigger = -10;
+// var xValues = [0, 2.95, 3.0, 3.05, 3.1, 3.15, 3.2, 3.25, 3.3];
+var xValues = [3.3, 3.25, 3.2, 3.15, 3.1, 3.05, 3.0, 2.95, 0];
+var logValues = [];
+var count = 0;
+let ebg = 0;
+let slope = 0;
+const set = new Set();
+let trigger = 0;
+let flag = 0;
+setTimeout(() => {
+  fillTable();
+}, 3700);
+
+function fillTable() {
+  filltableintrval = setInterval(() => {
+    if (localStorage.getItem("fullScreen") == "true") {
+      snackbarFunction(
+        "Put the key and press on the Power Supply button and Heater button to begin."
+      );
+      localStorage.setItem("fullScreen", false);
+      setTimeout(() => {
+        snackbarFunction(
+          "Readings are automatically recorded in the Table and Graph will be plotted."
+        );
+      }, 13000);
+    }
+
+    var rowData = JSON.parse(localStorage.getItem("rowData"));
+    if (rowData.tempc && rowData.sno < 8) {
+      srno = document.getElementsByClassName("srno")[rowData.sno];
+      tempc = document.getElementsByClassName("tempc")[rowData.sno];
+      tempk = document.getElementsByClassName("tempk")[rowData.sno];
+      curr = document.getElementsByClassName("current")[rowData.sno];
+      tsqr = document.getElementsByClassName("tempsqr")[rowData.sno];
+      tinv = document.getElementsByClassName("tempinv")[rowData.sno];
+      log = document.getElementsByClassName("log")[rowData.sno];
+
+      let temp;
+      srno.value = rowData.sno + 1;
+      tempc.value = rowData.tempc;
+      tempk.value = rowData.tempc + 273;
+
+      temp = rowData.curr;
+      curr.value = temp.toFixed(2);
+
+      temp = Math.pow(rowData.tempc + 273, 2);
+      tsqr.value = temp.toFixed(0);
+
+      temp = Math.pow(rowData.tempc + 273, -1) * 1000;
+      tinv.value = temp.toFixed(4);
+
+      temp = Math.log10(rowData.curr / Math.pow(rowData.tempc + 273, 2));
+      log.value = temp.toFixed(3);
+
+      if (currtrigger < rowData.tempc) {
+        currtrigger = rowData.tempc;
+        logValues.push(temp.toFixed(3));
+        console.log(logValues);
+        // drawGraph()
+        myChart.update();
+        count++;
+      }
+
+      // logValues=[...set];
+      // set.add(temp.toFixed(3));
+      // if(set.size > trigger){
+      //   trigger = set.size;
+
+      //   console.log(logValues);
+      //   myChart.update();
+      //   count++;
+      // }
+
+      let f = 0;
+      if (count == 8) {
+        if (f == 0) {
+          f = 1;
+          snackbarFunction(
+            "For Calculation Take the Value of Slope from graph "
+          );
+        }
+        document.querySelector(".slope-div").style.display = "block";
+        // document.querySelector("#download").style.display = "block";
+      }
+    }
+    if (rowData.sno == 8) {
+      clearInterval(filltableintrval);
+    }
+  }, 500);
+}
+
+// let ctx = document.getElementById("myChart").getContext("2d");
+// let myChart = new Chart(ctx, {
+//   type: "line",
+//   data: {
+//     labels: xValues,
+//     datasets: [
+//       {
+//         label: "log(Is / T^2) vs 1/T",
+//         data: logValues,
+//         borderColor: "rgba(75, 192, 192, 1)",
+//         backgroundColor: "rgba(75, 192, 192, 0.2)",
+//         borderWidth: 2,
+//         fill: false,
+//       },
+//     ],
+//   },
+//   options: {
+//     scales: {
+//       xAxes: [
+//         {
+//             scaleLabel: {
+//             display: true,
+//             labelString: "1/T (1/K)",
+//           },
+//         },
+//       ],
+//       yAxes: [
+//         {
+//             ticks: {
+//                 beginAtZero: true, // Start y-axis from 0
+//               },
+//             scaleLabel: {
+//             display: true,
+//             labelString: "log(Is / T^2)",
+//             beginAtZero: true,
+//           },
+//         },
+//       ],
+//     },
+//     responsive: true,
+//     maintainAspectRatio: false,
+//     animation:{
+//         duration:1
+//     }
+//   },
+// });
+let ctx = document.getElementById("myChart").getContext("2d");
+let myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [
+      {
+        label: "log(Is / T^2) vs 1/T",
+        data: logValues.reverse(),
+        borderColor: "rgba(75, 192, 192, 1)",
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        borderWidth: 2,
+        fill: false,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      xAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: "1/T (1/K)",
+          },
+          position: "top", // Position x-axis at the top
+          ticks: {
+            reverse: true, // Reverse the x-axis
+          },
+        },
+      ],
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true, // Start y-axis from 0
+            reverse: false, // Reverse the y-axis
+          },
+          scaleLabel: {
+            display: true,
+            labelString: "log(Is / T^2)",
+          },
+        },
+      ],
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    animation: {
+      duration: 1,
+    },
+  },
+});
+
+document.querySelector(".calcslope").addEventListener("click", calslope);
+function calslope() {
+  let x1, x2, y1, y2;
+  let slopevalue = document.querySelector(".slopev");
+  document.querySelector(".svalue").style.display = "block";
+  x1 = xValues[1];
+  x2 = xValues[4];
+
+  y1 = logValues[1];
+  y2 = logValues[4];
+
+  slope = (y2 - y1) / (x2 - x1);
+  ebg = 2.303 * 8.62 * Math.pow(10, -5) * slope * 1000;
+  slopevalue.innerHTML = slope.toFixed(4);
+  // document.querySelector(".ebg").style.display = "block";
+}
+
+document.querySelector(".ebgbtn").addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+  ebgcal();
+});
+
+function ebgcal() {
+  let slopeinp = document.querySelector(".sliderValue");
+  // let res = document.querySelector(".ebgvalue");
+  if (slopeinp.value === "") {
+    alert("Enter slope");
+  } else {
+    let result = 2.303 * 8.62 * Math.pow(10, -5) * slopeinp.value * 1000;
+    document.querySelector(".ebgres").style.display = "block";
+    res.innerHTML = result.toFixed(4);
+  }
+}
+
+snackbarFunction(
+  "Follow the Indicators and Click on the Terminals to make the connection."
+);
+
+function snackbarFunction(instruction) {
+  var x = document.getElementById("snackbar");
+  x.textContent = instruction;
+  x.className = "show";
+  setTimeout(function () {
+    x.className = x.className.replace("show", "");
+  }, 10000);
+}
+
+var elem = document.getElementsByTagName("body")[0];
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+async function downloadGraph() {
+  const { jsPDF } = window.jspdf;
+  const doc = new jsPDF();
+  // Set background color
+  doc.setFillColor(0, 123, 255); // Blue color (RGB)
+  doc.rect(10, 5, 190, 10, "F");
+  // Add a header with black text
+  doc.setFont("helvetica", "bold");
+  doc.setTextColor(255, 255, 255); // Set text color to black
+  doc.setFontSize(20); // Set font size for the header
+  doc.text("Graph", 75, 12);
+
+  const chartImage = myChart.toBase64Image();
+
+  doc.addImage(chartImage, "PNG", 25, 150, 150, 120);
+}
+
+async function downloadGraphAndObservations() {
+  try {
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF();
+
+    // Set background color
+    doc.setFillColor(0, 123, 255); // Blue color (RGB)
+    doc.rect(10, 5, 190, 10, "F");
+    // Add a header with black text
+    doc.setFont("helvetica", "bold");
+    doc.setTextColor(255, 255, 255); // Set text color to black
+    doc.setFontSize(20); // Set font size for the header
+    doc.text("Observations Table", 75, 12); // Add text at x=10, y=10
+
+    const tableCanvas = await html2canvas(document.querySelector("#table1"), {
+      scale: 2,
+    });
+    const tableImgData = tableCanvas.toDataURL("image/png");
+    doc.addImage(tableImgData, "PNG", 15, 17, 180, 120);
+
+    // Add the graph
+    const chartImage = myChart.toBase64Image();
+    // doc.addPage();
+
+    // //Add the graph head
+    // Set background color
+    doc.setFillColor(0, 123, 255); // Blue color (RGB)
+    doc.rect(10, 140, 190, 10, "F");
+    // Add a header with black text
+    doc.setFont("helvetica", "bold");
+    doc.setTextColor(255, 255, 255); // Set text color to black
+    doc.setFontSize(20); // Set font size for the header
+    doc.text("Graph", 95, 147); // Add text at x=10, y=10
+
+    doc.addImage(chartImage, "PNG", 25, 150, 150, 120);
+
+    doc.addPage();
+    //calculation page
+    //Add the labels
+    doc.setFillColor(0, 123, 255); // Blue color (RGB)
+    doc.rect(10, 5, 190, 10, "F");
+    // Add a header with black text
+    doc.setFont("helvetica", "bold");
+    doc.setTextColor(255, 255, 255); // Set text color to black
+    doc.setFontSize(20); // Set font size for the header
+    doc.text("Calculation", 75, 12);
+
+    document.querySelector(".calcbtn").style.display = "none";
+    const calc = await html2canvas(document.querySelector(".formula"), {
+      scale: 2,
+    });
+    const calcimg = calc.toDataURL("image/png");
+    doc.addImage(calcimg, "PNG", 15, 17, 180, 80);
+
+    // Save the PDF
+    doc.save("observations_and_graph.pdf");
+    document.querySelector(".calcbtn").style.display = "block";
+  } catch (error) {
+    console.log(error.message);
+    if (error.message === "Incomplete or corrupt PNG file") {
+      Swal.fire({
+        position: "top-end",
+        icon: "warning",
+        title: "Please complete the calculation using the Graph",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    }
+  }
+}
+
+// Add event listener to the download button
+document
+  .getElementById("download")
+  .addEventListener("click", downloadGraphAndObservations);
+document
+  .getElementById("downloadgraph")
+  .addEventListener("click", downloadGraph);
+document
+  .getElementById("inlineFormSelectPref")
+  .addEventListener("change", function () {
+    var contentUrl = this.value;
+    document.getElementById("main-svg").data = contentUrl;
+    // localStorage.setItem("diodetype", contentUrl);
+  });
+
+function clearTableInputs() {
+  //chart
+  myChart.data.datasets.forEach((dataset) => {
+    dataset.data = [];
+  });
+  // Update the chart to reflect the changes
+  myChart.update();
+
+  // Get the table
+  const table = document.getElementById("table1");
+
+  // Get all input elements within the table
+  const inputs = table.getElementsByTagName("input");
+
+  // Loop through each input and clear its value
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].value = "";
+  }
+}
+
+document
+  .getElementById("inlineFormSelectPref")
+  .addEventListener("change", function () {
+    const selectElement = document.getElementById("inlineFormSelectPref");
+    const selectedOption = selectElement.options[selectElement.selectedIndex];
+    const contentUrl = this.value;
+
+    // Disable the selected option
+    // selectedOption.disabled = true
+    // Store the selected value in localStorage
+    localStorage.setItem("diodetype", contentUrl);
+    // selectElement.disabled = true;
+    // Optional: Log the stored value for verification
+    console.log("Selected and stored value:", contentUrl);
+    clearTableInputs();
+  });
+
+function updateValue(value) {
+  document.getElementById("sliderValue").textContent = value;
+}
